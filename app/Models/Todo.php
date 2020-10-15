25c4619937b44b03cea89dcff0fd025570c84b5d
shyan26 <shidong.yan@ingka.com>
2020-10-15 16:24:01 +0800

    c0:TodoModel 增删改，以及done,recover

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -4,8 +4,50 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use phpDocumentor\Reflection\Types\Array_;
+use phpDocumentor\Reflection\Types\Integer;
 
 class Todo extends Model
 {
     use HasFactory;
+
+    /**
+     * 保存这个Todo Task
+     * @param Todo $todo
+     */
+    public static function insertTodoTask(Todo $todo){
+        $todo->save();
+    }
+
+    /**
+     * 更新一个TodoTask信息
+     * @param array $todo
+     */
+    public static function updateTodoTask(array $todo){
+        Todo::query()->find($todo['id'])->update($todo);
+    }
+
+    /**
+     * 移除一个 TodoTask
+     * @param int $id
+     * @throws \Exception
+     */
+    public static function removeTodoTask(int $id){
+        Todo::query()->find($id)->delete();
+    }
+    /**
+     * 已完成Todo
+     * @param Integer $todoId
+     */
+    public static function done(Integer $todoId){
+        Todo::query()->find($todoId)->save(['done_at'=>time()]);
+    }
+
+    /**
+     * 恢复Todo
+     * @param Integer $todoId
+     */
+    public static function recover(Integer $todoId){
+        Todo::query()->find($todoId)->save(['done_at'=>0]);
+    }
 }
